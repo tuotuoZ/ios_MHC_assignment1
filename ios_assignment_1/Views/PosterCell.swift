1e6796d595dca2ee554b2821a46d06fa45251bb9
Tony <bz15@hampshire.edu>
2018-09-24 18:14:42 -0400

    new assginment 2 till Collection View Setup

---
+++ ios_assignment_1/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  ios_assignment_1
+//
+//  Created by Guest User on 9/24/18.
+//  Copyright © 2018 Tony. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
